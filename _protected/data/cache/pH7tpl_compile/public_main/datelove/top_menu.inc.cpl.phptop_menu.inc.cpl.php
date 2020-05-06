e111bf4f679630c23f304a3b4465def7dc5f54bd
root <root@ip-172-31-45-114.us-east-2.compute.internal>
2020-05-06 21:40:55 +0000

    first commit

---
+++ _protected/data/cache/pH7tpl_compile/public_main/datelove/top_menu.inc.cpl.phptop_menu.inc.cpl.php
@@ -0,0 +1,19 @@
+<?php 
+namespace PH7;
+defined('PH7') or exit('Restricted access');
+/*
+Created on 2020-05-06 20:33:08
+Compiled file from: /var/www/html/ph7builder/templates/themes/datelove/tpl/top_menu.inc.tpl
+Template Engine: PH7Tpl version 1.4.0 by Pierre-Henry Soria
+*/
+/***************************************************************************
+ *     pH7CMS Web Engineer: Pierre-Henry Soria
+ *               --------------------
+ * @since      Mon Mar 21 2011
+ * @author     Pierre-Henry Soria
+ * @email      hello@ph7cms.com
+ * @link       https://ph7cms.com
+ * @copyright  (c) 2011-2020, Pierre-Henry Soria. All Rights Reserved.
+ * @license    Creative Commons Attribution 3.0 License - http://creativecommons.org/licenses/by/3.0/
+ ***************************************************************************/
+?><?php $this->display('top_menu.inc.tpl', PH7_PATH_TPL . PH7_DEFAULT_THEME . PH7_DS); ?>
